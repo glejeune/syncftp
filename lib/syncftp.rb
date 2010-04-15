99a1940edb44244f07d34f112be37505432727f4
glejeune <gregoire.lejeune@free.fr>
2010-04-15 15:32:38 +0200

    Bug correction and documentation

--- lib/syncftp.rb
+++ lib/syncftp.rb
@@ -1,19 +1,87 @@
+# Copyright (c) 2009 Gregoire Lejeune
+# 
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 require 'net/ftp'
 require 'yaml'
 require 'digest/md5'
 require 'tmpdir'
+require 'logger'
 require 'rubygems'
 require 'mime/types'
 
 module Net
   class FTP
-    def remote_exist?( file )
+    #
+    # Net::FTP extension
+    #
+    # Check if the +file+ exist on the remote FTP server
+    #
+    def remote_file_exist?( file )
       ls( file ).size != 0
+    end
+
+    #
+    # Net::FTP extension
+    #
+    # Check if the +dir+ exist on the remote FTP server
+    #
+    def remote_dir_exist?( dir )
+      path = dir.split( "/" )
+      find = path.pop
+      path = path.join( "/" )
+      path = "." if path == ""
+      
+      nlst( path ).include?( dir )
+    end
+    
+    #
+    # Net::FTP extension
+    #
+    # Like FileUtils.mkdir_p but for Net::FTP
+    #
+    def mkdir_p( dir )
+      path = dir.split( "/" )
+      mkpath = path.shift
+      begin
+        mkdir( mkpath ) unless mkpath == ""
+      rescue Net::FTPPermError => e
+        raise Net::FTPPermError, e.message, caller unless remote_dir_exist?(mkpath)
+      end
+      path.each do |d|
+        mkpath = [mkpath, d].join( "/" )
+        begin
+          mkdir( mkpath )
+        rescue Net::FTPPermError => e
+          raise Net::FTPPermError, e.message, caller unless remote_dir_exist?(mkpath)
+        end
+      end
     end
   end
 end
 
 class File
+  #
+  # File extension
+  #
+  # Check if the +file+ is a binary file
+  #
   def self.binary?( file )
     MIME::Types.type_for( file ).map{ |e| (e.binary?) ? e : nil }.compact.size > 0
   end
@@ -27,20 +95,29 @@ class SyncFTP
   #
   # you can specify :
   #
-  #  :username - default = "anonymous"
-  #  :password - default = nil
-  #  :port - default = 21
+  # * +:username+ - default = "anonymous"
+  # * +:password+ - default = nil
+  # * +:port+ - default = 21
+  # * +:logfile+ - default = STDOUT
+  # * +:loglevel+ - default = Logger::UNKNOWN (Cool if you don't want logs)
   #
   def initialize(host, options = {})
-    options = {:username => "anonymous", :password => nil}.merge(options)
+    options = {:username => "anonymous", :password => nil, :logfile => STDOUT, :loglevel => Logger::UNKNOWN}.merge(options)
     @host, @port = host, options[:port]||21
     @username, @password = options[:username], options[:password]
     @remote_md5s = {} 
     @local_md5s = {}
+    @log = Logger.new( options[:logfile] )
+    @log.level = options[:loglevel]
   end
   
   #
   # Sync local to remote
+  #
+  # you can specify :
+  #
+  # * +:local+ : the local directory (default = ".")
+  # * +:remote+ : the remote directory (default = ".")
   #
   def sync( options = {} )
     options = { :local => ".", :remote => "." }.merge( options )
@@ -53,7 +130,7 @@ class SyncFTP
         ftp.gettextfile( remote+"/"+".syncftp", tmpname )
         @remote_md5s = YAML.load( File.open( tmpname ).read )
       rescue Net::FTPPermError => e
-        raise Net::FTPPermError, e.message, caller if ftp.remote_exist?( remote+"/"+".syncftp" )
+        raise Net::FTPPermError, e.message, caller if ftp.remote_file_exist?( remote+"/"+".syncftp" )
       end
       
       # Do the job Bob !
@@ -69,7 +146,7 @@ class SyncFTP
   end
   
   private
-  def tmpfilename
+  def tmpfilename #:nodoc:
     tmpdir = Dir::tmpdir
     basename = File.basename( $0 )
     tempname = nil
@@ -89,7 +166,7 @@ class SyncFTP
     return tmpname
   end
   
-  def connect
+  def connect #:nodoc:
     ftp = Net::FTP.new( )
     ftp.connect( @host, @port )
     ftp.login( @username, @password )
@@ -97,12 +174,8 @@ class SyncFTP
     ftp.close
   end
   
-  def send_dir(ftp, local, remote)
-    begin
-      ftp.mkdir(remote)
-    rescue Net::FTPPermError => e
-      raise Net::FTPPermError, e.message, caller unless ftp.remote_exist?(remote)
-    end
+  def send_dir(ftp, local, remote) #:nodoc:
+    ftp.mkdir_p(remote)
     
     Dir.foreach(local) do |file|
       next if file == "." or file == ".."
@@ -117,7 +190,7 @@ class SyncFTP
         @local_md5s[remote_file] = Digest::MD5.hexdigest( File.open(local_file).read )
 
         if( @local_md5s[remote_file] != @remote_md5s[remote_file] )
-          # puts "-- copy #{local_file} => #{remote_file}"
+          @log.info "Copy #{local_file} to ftp://#{@host}:#{@port}/#{remote_file}"
           
           # It's a file, we just send it
           if File.binary?(local_file)
@@ -125,6 +198,8 @@ class SyncFTP
           else
             ftp.puttextfile(local_file, remote_file)
           end
+        else
+          @log.info "#{local_file} don't need to be overwritten !"
         end
       end
     end
